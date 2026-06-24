0916dfd5ed307ef1a89d1873ad24afe9e4e19b39
Lv Peng <lvpeng990324@qq.com>
2026-06-24 15:29:58 +0800

    解决lint报错

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react-hooks/purity */
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
